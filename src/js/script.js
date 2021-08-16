d3891a6d17eefc407ee35b7fa380120cf46060a8
Diego Ocaranza <diego.ocaranza@outlook.com>
2021-08-16 19:41:02 -0400

    comienzo de desarrollo de funcionalidades para la página

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,38 @@
 const btnMenu = document.getElementById("menuButton");
-
 const menu = document.getElementById("mainNav");
+const positionMenu = document.getElementById("header");
+const scrollButton = document.getElementById("upButton");
+const home = document.getElementById("homeLimit");
+
+//MOSTRAR MENU
 function mostrarMenu(){
     menu.classList.toggle("mostrar");
 }
 
+//CAMBIA COLOR HEADER
+function positionHeader() {
+    console.log(window.scrollY);
+    if(window.scrollY > 30){
+        header.classList.add("headerColor");
+    }else{
+        header.classList.remove("headerColor");
+    }
+}
+
+//BOTON SUBIR
+function scrollWindowTop() {
+    let offsetMain = home.offsetTop;
+    window.scrollTo(0,offsetMain);
+}
+
+function visibilityUpButton() {
+    if(window.scrollY > 200) {
+        scrollButton.classList.remove("hidden");
+    }else{
+        scrollButton.classList.add("hidden");
+    }
+}
+
+//INVOCAR FUNCIONES
 btnMenu.addEventListener("click", mostrarMenu);
+document.addEventListener("scroll", positionHeader);
